1ffe3edb9908eed6537c9e56e66e21215d5a3e6e
a19980427 <1332587753@qq.com>
2022-10-27 11:14:41 +0800

    fk

--- mmdetection-master/demo/my_test.py
+++ mmdetection-master/demo/my_test.py
@@ -1,13 +1,6 @@
-import mmcv
-import mmdet
-from mmcv.ops import RoIPool as poll
-
 if __name__ == '__main__':
     import torch
 
-    print("hello world")
-    print("why why why ")
-    print("goasd")
-    print("help")
+    print("fkfkfkfkfk")
 
     print(torch.__version__)
